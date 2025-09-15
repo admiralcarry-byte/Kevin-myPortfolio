be0364b4ae9eed3da7a9286ac51bc857a76b029e
wilhelmleonardo167-cloud <wilhelmleonardo167@gmail.com>
2025-09-15 12:26:47 -0400

    my

---
+++ src/data/skills.js
@@ -0,0 +1,100 @@
+const SkillsData = [
+  {
+    name: "HTML",
+    image: require("../assets/Images/html.png"),
+  },
+  {
+    name: "CSS",
+    image: require("../assets/Images/css.png"),
+  },
+  {
+    name: "JavaScript",
+    image: require("../assets/Images/javascript.png"),
+  },
+  {
+    name: "React",
+    image: require("../assets/Images/react.png"),
+  },
+  {
+    name: "MongoDB",
+    image: require("../assets/Images/mongodb.png"),
+  },
+  {
+    name: "Express",
+    image: require("../assets/Images/express.png"),
+  },
+  {
+    name: "Node",
+    image: require("../assets/Images/node.png"),
+  },
+  {
+    name: "Php",
+    image: require("../assets/Images/php.png"),
+  },
+  {
+    name: "Laravel",
+    image: require("../assets/Images/laravel.png"),
+  },
+  {
+    name: "Redux",
+    image: require("../assets/Images/redux.png"),
+  },
+  {
+    name: "Next",
+    image: require("../assets/Images/next.png"),
+  },
+  {
+    name: "Tailwind",
+    image: require("../assets/Images/tailwind.png"),
+  },
+  {
+    name: "Android",
+    image: require("../assets/Images/android.png"),
+  },
+  {
+    name: "AWS",
+    image: require("../assets/Images/aws.png"),
+  },
+  {
+    name: "MySQL",
+    image: require("../assets/Images/mysql.png"),
+  },
+  {
+    name: "PostgreSQL",
+    image: require("../assets/Images/postgresql.png"),
+  },
+  {
+    name: "Java",
+    image: require("../assets/Images/java.png"),
+  },
+  {
+    name: "AI",
+    image: require("../assets/Images/ai.png"),
+  },
+  {
+    name: "Python",
+    image: require("../assets/Images/python.png"),
+  },
+  {
+    name: "Django",
+    image: require("../assets/Images/django.png"),
+  },
+  {
+    name: "FastAPI",
+    image: require("../assets/Images/fastapi.png"),
+  },
+  {
+    name: "GitHub",
+    image: require("../assets/Images/github.png"),
+  },
+  {
+    name: "Vue",
+    image: require("../assets/Images/vue.png"),
+  },
+  {
+    name: "CRM",
+    image: require("../assets/Images/crm.png"),
+  },
+];
+
+export default SkillsData;
